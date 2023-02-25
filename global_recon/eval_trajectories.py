fixed std calculation

global_recon/eval_trajectories.py
@@ -11,6 +11,7 @@ import glob
 from torch.utils.data import DataLoader
 from motion_infiller.data.amass_dataset import AMASSDataset
 from lib.utils.tools import worker_init_fn, find_last_version, get_checkpoint_path
+from lib.utils.tools import AverageMeter, concat_lists, find_consecutive_runs
 
 results_dir = './out/vis_traj_pred'
 
@@ -24,13 +25,15 @@ else:
 torch.torch.set_grad_enabled(False)
 
 models = ["MOJO", "GLAMR"]
-#models = ["GLAMR"]
+models = ["GLAMR"]
 models = ["MOJO"]
 
 test_dataset = AMASSDataset('../GLAMR/datasets/amass_processed/v1', 'test', None, training=False, seq_len=800, ntime_per_epoch=int(2e6))
 test_dataloader = DataLoader(test_dataset, shuffle=False, batch_size=1, num_workers=0, pin_memory=True, worker_init_fn=worker_init_fn)
 
 for model in models:
+
+    print(f"evaluating {model}")
 
     evaluator = Evaluator(results_dir, 'amass', device=device, log_file=f'{results_dir}/log_traj_{model}.txt', compute_sample=multi_seeds)
     seed_evaluator = Evaluator(results_dir, 'amass', device=device, log_file=f'{results_dir}/log_traj_seed_{model}.txt', compute_sample=multi_seeds)
@@ -83,18 +86,25 @@ for model in models:
             continue
 
         for seed in seeds:
-            print("seed ", seed)
 
             batch['smpl_orient_world'] = results['infer_out_orient'][0, seed]
             batch['smpl_pose'] = results['infer_out_pose'][0, seed, :, 3:]
             batch['root_trans_world'] = results['infer_out_trans'][0, seed]
 
             data["person_data"] = {0: batch}
+
             metrics_dict = seed_evaluator.compute_sequence_metrics(data, seq_name, accumulate=False)
             metrics_dict_arr.append(metrics_dict)
 
+            print("seed ", seed, metrics_dict["metrics"])
+
         metrics_dict_allseeds = evaluator.metrics_from_multiple_seeds(metrics_dict_arr)
         evaluator.update_accumulated_metrics(metrics_dict_allseeds, seq_name)
+
+        full_data = torch.cat([results['infer_out_orient'][0, :],results['infer_out_pose'][0, :, :, 3:],results['infer_out_trans'][0, :]], dim=-1)
+
+        metrics_dict_allseeds["metrics"]["std"] = AverageMeter(seed_evaluator.compute_std(full_data))
+
         evaluator.print_metrics(metrics_dict_allseeds, prefix=f'{sind}/{len(test_dataloader)} --- All seeds {seq_name} --- ', print_accum=False)
 
     evaluator.print_metrics(prefix=f'Total ------- ', print_accum=True)

global_recon/utils/evaluator_traj.py
@@ -111,16 +111,6 @@ def compute_pelvis_accel_error(data):
     return accel_err.item(), info
 
 
-def compute_std(data):
-    num_data = 0
-    std_err = 0
-    for idx, pose_dict in data['person_data'].items():
-        jpos = pose_dict['eval_joints_world'][:,0:1,:]        
-        std_err = torch.std(jpos)
-        num_data += jpos.shape[0]
-    std_err /= num_data # J: not sure if necessary to divide by sequence length
-    info = {'num_data': num_data}
-    return std_err.item(), info
 
 
 class Evaluator:
@@ -140,12 +130,15 @@ class Evaluator:
             'ACCEL': compute_accel_error,
             'G-pelvis': compute_Global_PE,
             'ACCEL-pelvis': compute_pelvis_accel_error,
-            'STD': compute_std,
         }
 
         self.metrics_name = list(self.metrics_func.keys())
-        self.seed_min_metrics = ['PA-MPJPE-invis']
         self.reset()
+
+    def compute_std(self, data):
+        # print(data.shape) # torch.Size([5, 800, 75])
+        std_err = torch.std(data, dim=0).sum()
+        return std_err
 
     def reset(self):
         self.metrics_dict_collection = dict()
@@ -309,11 +302,13 @@ class Evaluator:
 
 
     def metrics_from_multiple_seeds(self, metrics_dict_arr):
-        print("metrics_from_multiple_seeds")
+
         metrics_dict = defaultdict(dict)
         metrics_dict['seq_len'] = metrics_dict_arr[0]['seq_len']
+
         for metric in self.metrics_name:
             if 'sample' in metric or 'mean' in metric:
+                # J: seems this does not apply to any of our metrics
                 val_arr = np.stack([x['metrics'][metric].avg for x in metrics_dict_arr])
                 num_data = metrics_dict_arr[0]['metrics'][metric].count
                 if num_data == 0:
@@ -326,14 +321,15 @@ class Evaluator:
                     val = val.mean()
                 metrics_dict['metrics'][metric] = AverageMeter(val, num_data)
             else:
+                # J: according to the paper it should be best-out-of-5
                 val_arr = np.array([x['metrics'][metric].avg for x in metrics_dict_arr])
                 num_data = metrics_dict_arr[0]['metrics'][metric].count
-                if metric in self.seed_min_metrics:
+                if metric in ['G-MPJPE', 'G-MPVE', 'ACCEL', 'G-pelvis', 'ACCEL-pelvis', 'STD']: # J: all metrics at the moment
                     val = val_arr.min()
                 else:
                     val = val_arr.mean()
                 metrics_dict['metrics'][metric] = AverageMeter(val, num_data)
-    
+                    
         return metrics_dict
     
 
@@ -341,7 +337,11 @@ class Evaluator:
         if metrics_dict is None:
             metrics_dict = self.acc_metrics_dict
         fmt_str = f"%s: %{fmt} (%{fmt})" if print_accum else f"%s: %{fmt}"
-        str_stats = f'{prefix}{self.algo} --- ' + ' '.join([fmt_str % ((x, y.avg, y.val) if print_accum else (x, y.avg)) for x, y in metrics_dict['metrics'].items() if not isinstance(y.avg, np.ndarray)])
+
+        str_stats = f'{prefix}{self.algo} --- ' 
+        for x, y in metrics_dict['metrics'].items():
+            if not isinstance(y.avg, np.ndarray):
+                str_stats += ' '.join([fmt_str % ((x, y.avg, y.val) if print_accum else (x, y.avg))  ]) + " "
 
 
         self.log.info(str_stats)